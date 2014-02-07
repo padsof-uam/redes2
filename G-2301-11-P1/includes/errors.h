c9103336157ac10cc6c584a63a0ceac68745e42a
VicdeJuan <dejuan.victor@gmail.com>
2014-02-07 12:52:01 +0100

    punto 3

--- G-2301-11-P1/includes/errors.h
+++ G-2301-11-P1/includes/errors.h
@@ -3,11 +3,12 @@
 
 #define OK 0
 
-#define ERR_TTOU 2
-#define ERR_TTIN 4
-#define ERR_TSTP 6
-#define ERR_HUP 8
+//#define ERR_TTOU 2
+//#define ERR_TTIN 4
+//#define ERR_TSTP 6
+//#define ERR_HUP 8
 
-#define ERR 1
+#define ERR -1
+#define SYS_ERR -2 
 
 #endif

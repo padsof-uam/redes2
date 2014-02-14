a08feb04d5220dc590673280d066ef9b4e45f1b2
VicdeJuan <dejuan.victor@gmail.com>
2014-02-14 13:02:17 +0100

    Trabajo de hoy. Poco productivo

---
+++ G-2301-11-P1/tests/test_listener.c
@@ -0,0 +1,31 @@
+#include "listener.h"
+#include "test_listener.h"
+#include "testmacros.h"
+#include <stdio.h>
+
+/* BEGIN TESTS */
+int t_Server_open_socket() {
+	int handler = Server_open_socket(DEFAULT_PORT, DEFAULT_MAX_QUEUE);
+	
+	mu_assert("Server won't open", handler!=-ERR_SOCK);
+
+	mu_end;
+}
+
+/* END TESTS */
+
+int test_listener_suite(int* errors, int* success) {
+	int tests_run = 0;
+	int tests_passed = 0;
+
+	printf("Begin listener suite.\n");
+/* BEGIN TEST EXEC */
+	mu_run_test(t_Server_open_socket);
+	
+/* END TEST EXEC */
+	printf("End listener suite. %d/%d\n\n", tests_passed, tests_run);
+
+	*errors += (tests_run - tests_passed);
+	*success += tests_passed;
+	return tests_run;
+}
---
+++ G-2301-11-P1/tests/test_listener.h
@@ -0,0 +1,10 @@
+#ifndef _testsuite__H
+#define _testsuite__H
+
+#define DEFAULT_PORT 1500
+#define DEFAULT_MAX_QUEUE 150
+
+
+int test_listener_suite(int*, int*);
+
+#endif

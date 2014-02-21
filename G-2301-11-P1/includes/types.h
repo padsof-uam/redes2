c54e8014c84b54698567623faa2f6a39cf3bd6c2
Guillermo Julián <gjulian93@gmail.com>
2014-02-21 15:38:38 +0100

    Estructura básica creada

---
+++ G-2301-11-P1/includes/types.h
@@ -0,0 +1,27 @@
+#include <sys/types.h>
+#include "dictionary.h"
+#include "list.h"
+
+#ifndef COMMSTRUCTS_H
+#define COMMSTRUCTS_H
+
+#define MAX_IRC_MSG 512
+
+struct irc_globdata {
+	dictionary* fd_user_map;
+	dictionary* nick_fd_cachemap;
+	list* chan_list;
+};
+
+struct sockcomm_data {
+	int fd;
+	char data[MAX_IRC_MSG];
+	ssize_t len;
+};
+
+struct msg_sockcommdata {
+	long msgtype;
+	struct sockcomm_data scdata;
+};
+
+#endif
---
+++ G-2301-11-P1/src/cmd_processor.c
@@ -0,0 +1,57 @@
+#include "cmd_processor.h"
+#include <stdlib.h>
+#include <sys/syslog.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/msg.h>
+#include "errors.h"
+#include "types.h"
+
+struct proc_thdata {
+	int rcv_qid;
+	int snd_qid;
+	msg_process pfun;
+};
+
+int create_proc_thread(pthread_t* pth, int rcv_qid, int snd_qid, msg_process pfun) {
+	struct proc_thdata *thdata = malloc(sizeof(struct proc_thdata));
+	thdata->rcv_qid = rcv_qid;
+	thdata->snd_qid = snd_qid;
+	thdata->pfun = pfun;
+
+	if(pthread_create(pth, NULL, proc_thread_entrypoint,thdata) < 0 )
+	{
+		syslog(LOG_CRIT, "Could not start proc_thread: %s", strerror(errno));
+		return ERR;
+	}
+
+	return OK;
+}
+
+static void _proc_thread_cleanup(void* thdata)
+{
+	free(thdata);
+}
+
+void* proc_thread_entrypoint(void* data) {
+	struct proc_thdata *thdata = (struct proc_thdata*) data;
+	struct msg_sockcommdata msg;
+	struct irc_globdata gdata;
+	pthread_cleanup_push(_proc_thread_cleanup, thdata);
+
+	while(1)
+	{
+		if(msgrcv(thdata->rcv_qid, &msg, sizeof(struct msg_sockcommdata), 0, 0) != -1)
+		{
+			thdata->pfun(thdata->snd_qid, &msg.scdata);
+		}
+		else
+		{
+			syslog(LOG_ERR, "error recibiendo de msgrcv: %s", strerror(errno));
+		}
+	}
+
+	pthread_exit(NULL);
+}
+
+

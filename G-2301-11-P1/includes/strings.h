bba432d6b63944bb6a523e99656c1b9dcba7af88
Guillermo Julián <gjulian93@gmail.com>
2014-03-09 12:49:12 +0100

    Cambiado el Makefile para soportar librerías.
    Me he tirado más con esta mierda que con cualquier otra cosa de la práctica.
    Increíbles comandos que dentro de diez minutos ya no entenderé. Pero bueno, no es recursivo, no hay que tocar nada al crear nuevas librerías, el orden de compilación parece no afectar mucho (igual me equivoco y esto luego peta fuerte).
    
    Ahora sólo falta hacer las librerías xD

--- G-2301-11-P1/includes/strings.h
+++ G-2301-11-P1/includes/strings.h
@@ -1,10 +1,26 @@
 #ifndef STRINGS_H
 #define STRINGS_H
 
+#include <sys/types.h>
+
 /**
- * Strip spaces from beginning and end of the string.
- * @param str String.
+ * Elimina caracteres de espacio del principio y final de la cadena.
+ * Además de espacios, se eliminan tabuladores y demás caracteres que no
+ * 	representan caracteres visibles.
+ * @see isspace(3)
+ * @param str Cadena.
  */
 void strip(char** str);
+
+/**
+ * Encuentra la primera ocurrencia de la cadena _find_ en _s_.
+ * @param  s    Cadena donde buscar.
+ * @param  find Cadena a buscar.
+ * @param  slen Longitud máxima que se buscará.
+ * @return      Puntero a la posición de inicio de _find_ en _s_ o NULL 
+ *                 si no se ha encontrado.
+ */
+char * strnstr(const char* s, const char* find, size_t slen);
+
 #endif
 
---
+++ G-2301-11-P1/srclib/libstrings/strings.c
@@ -0,0 +1,45 @@
+#include "strings.h"
+
+#include <ctype.h>
+#include <string.h>
+
+void strip(char **str)
+{
+    char *end;
+
+    if (!str || !(*str))
+        return;
+
+    while (isspace(**str))
+        (*str)++;
+
+    end = strchr(*str, '\0');
+    end--;
+
+    while (isspace(*end))
+    {
+        *end = '\0';
+        end--;
+    }
+}
+
+char * strnstr(const char* s, const char* find, size_t slen)
+{
+    char c, sc;
+    size_t len;
+
+    if ((c = *find++) != '\0') {
+        len = strlen(find);
+        do {
+            do {
+                if ((sc = *s++) == '\0' || slen-- < 1)
+                    return (NULL);
+            } while (sc != c);
+            if (len > slen)
+                return (NULL);
+        } while (strncmp(s, find, len) != 0);
+        s--;
+    }
+    return ((char *)s);
+}
+

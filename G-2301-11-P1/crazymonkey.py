1e474d808581191ca2af9dc94eddc69f784f2139
Guillermo Julián <gjulian93@gmail.com>
2014-03-10 20:57:47 +0100

    Control manual en crazymonkey

--- G-2301-11-P1/crazymonkey.py
+++ G-2301-11-P1/crazymonkey.py
@@ -7,17 +7,22 @@ import string
 import time
 import sys
 import signal
+import math
 
 host = "localhost"
 port = 6667
 delay = 0.1
-client_spawndelay = 0.11
-client_batch = 5
-client_max = 200
-batch_time_sec = 5
+client_spawndelay = 0.01
+client_batch = 50
+client_max = 500
+batch_time_sec = 1
 ping_interval = 0.5
+threshold_manual_control = 250
+enable_sending = False
 
 response_times = []
+partial_averages = []
+partial_devs = []
 threads = []
 
 users = []
@@ -65,7 +70,7 @@ class IRCThread(threading.Thread):
 			msgargs['user'] = randitem(users)
 			msgargs['channel'] = randitem(channels)
 
-			if sock.send(command.format(**msgargs)) == 0:
+			if enable_sending and sock.send(command.format(**msgargs)) == 0:
 				print 'error sending'
 
 			time.sleep(delay)
@@ -81,8 +86,7 @@ def signal_handler(signal, frame):
 	for t in threads:
 		t.stop()
 
-	png.close()
-	sys.exit(0)
+	stop = True
 
 users = [randstr(8) for _ in range(100)]
 channels = [randstr(5) for _ in range(10)]
@@ -97,14 +101,15 @@ client_count = 0
 batch_elapsed = 0
 batch_num = 0
 error = False
+stop = False
 
-while not error and client_count + client_batch < client_max:
+while not error and not stop and client_count <= client_max:
 	batch_num = batch_num + 1
 
 	print "batch #{0}: {1} clients".format(batch_num, client_count)
 	batch_start = len(response_times)
 
-	while not error and batch_elapsed < batch_time_sec:
+	while not error and not stop and batch_elapsed < batch_time_sec:
 		start = time.clock()
 		png.send("PING\r\n")
 		rcv = png.recv(6)
@@ -118,16 +123,36 @@ while not error and client_count + client_batch < client_max:
 		batch_elapsed = batch_elapsed + ping_interval
 		time.sleep(ping_interval)
 
-	for i in xrange(0,client_batch):
-		th = IRCThread()
-		th.start()
-		time.sleep(client_spawndelay)
-		threads.append(th)
+	if threshold_manual_control != -1 and threshold_manual_control <= client_count:
+		next_str = 'a'
+
+		while not next_str.strip().isdigit() and next_str != '-1':
+			next_str = raw_input('Manual control enabled. Enter number of clients to create (or -1 to end): ')
+
+		if next_str == '-1':
+			stop = True
+			continue
+
+		client_batch = int(next_str)
+		
+	if client_count < client_max:
+		for i in xrange(0,client_batch):
+			th = IRCThread()
+			th.start()
+			time.sleep(client_spawndelay)
+			threads.append(th)
 
 	client_count = client_count + client_batch
 	batch_elapsed = 0
 
-	print 'average ping for this batch: ', avg(response_times[batch_start:]), 'ms'
+	batch_times = response_times[batch_start:]
+	mean = avg(batch_times)
+	sdev = math.sqrt(avg([x * x for x in batch_times]) - mean * mean)
+
+	partial_averages.append(mean)
+	partial_devs.append(sdev)
+
+	print 'ping times for this batch: {0} +/- {1} ms'.format(mean, sdev)
 
 png.close()
 
@@ -138,5 +163,9 @@ for t in threads:
 	t.stop()
 
 out = open('cm-times.dat','w')
-out.writelines([str(x) for x in response_times])
+out.write('\n'.join([str(x) for x in response_times]))
 out.close()
+
+pout = open('cm-partials.dat', 'w')
+pout.write('\n'.join(['{0}\t{1}'.format(m, d) for m,d in zip(partial_averages, partial_devs)]))
+pout.close()

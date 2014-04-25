2bab0541dbc94b2472b921da1c87ab7e2498c1bd
VicdeJuan <dejuan.victor@gmail.com>
2014-04-25 17:31:21 +0200

    FTP documentado

--- G-2301-11-P2/includes/irc_ftp.h
+++ G-2301-11-P2/includes/irc_ftp.h
@@ -12,14 +12,14 @@ typedef void (*ftp_callback)(ftp_status);
 /**
  * Estructura para la creación de hilos manejadores de ftp.
  */
-struct th_ftpdata
-{
-	FILE * f;
-	int sock;
-	ftp_callback cb;
-	int size;
-	struct ftp_connection * data;
-};
+ struct th_ftpdata
+ {
+ 	FILE * f;
+ 	int sock;
+ 	ftp_callback cb;
+ 	int size;
+ 	struct ftp_connection * data;
+ };
 
 
 /**
@@ -30,7 +30,7 @@ struct th_ftpdata
  * @param  recv_ftp_th Identificador del hilo que se creará para la recepción.
  * @return             OK o ERR.
  */
-int ftp_wait_file(const char* dest, int* port, ftp_callback cb, pthread_t * recv_ftp_th);
+ int ftp_wait_file(const char* dest, int* port, ftp_callback cb, pthread_t * recv_ftp_th);
 
 /**
  * Envía un fichero.
@@ -41,7 +41,7 @@ int ftp_wait_file(const char* dest, int* port, ftp_callback cb, pthread_t * recv
  * @param  snd_ftp_th	Identificador del hilo que se creará para la recepción.
  * @return      OK/ERR.
  */
-int ftp_send_file(const char* file, uint32_t ip, int port, ftp_callback cb,	pthread_t * snd_ftp_th);
+ int ftp_send_file(const char* file, uint32_t ip, int port, ftp_callback cb,	pthread_t * snd_ftp_th);
 
 
 #endif
